29cc3a3b76f19c029ec62ea69e14bfc569affab6
Winston Ong <obdwinston@gmail.com>
2023-12-25 18:52:35 +0800

    added transitions

--- scripts.js
+++ scripts.js
@@ -6,12 +6,25 @@ for (i = 0; i < questions.length; i++) {
     this.classList.toggle("active");
 
     var image = this.querySelector("img");
-    if (image.src.split("/").pop() === "icon-plus.svg")
+    if (this.classList.contains("active"))
       image.src = "assets/images/icon-minus.svg";
     else image.src = "assets/images/icon-plus.svg";
 
     var answer = this.nextElementSibling;
-    if (answer.style.display === "block") answer.style.display = "none";
-    else answer.style.display = "block";
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
   });
 }
+
+window.onload = function () {
+  var firstQuestion = questions[0];
+  var firstAnswer = firstQuestion.nextElementSibling;
+
+  firstQuestion.classList.toggle("active");
+  firstQuestion.querySelector("img").src = "assets/images/icon-minus.svg";
+
+  firstAnswer.style.maxHeight = firstAnswer.scrollHeight + "px";
+};
